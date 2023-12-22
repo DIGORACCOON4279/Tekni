66497ad4a8a61edf2c6caa465ad3fbed69b4c9fe
DIGORACCOON4279 <dilema4000@gmail.com>
2023-12-22 16:05:30 -0500

    Updating README

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,15 +12,15 @@ const NavBar = () => {
                 {/* <label for="menu">
                 </label> */}
                 <ul>
-                    <li><img src="./img/home.svg" alt=""/>Home</li>
-                    <li><img src="./img/products.svg" alt=""/>Products</li>
+                    <li><img src="./img/home.svg" alt="Home icon"/>Home</li>
+                    <li><img src="./img/products.svg" alt="Products icon"/>Products</li>
                     <li id="categorySection"><img src="./img/categories.svg" alt=""/>Categories
                         <nav className="categoryItems">
                             <ul>
-                                <li className="li" ><img src="./img/laptop6.webp" alt=""/>Laptops</li>
-                                <li className="li" ><img src="./img/smartphone6.webp" alt=""/>Smartphones</li>
-                                <li className="li" ><img src="./img/tv8.webp" alt=""/>SmartTVs</li>
-                                <li className="li" ><img src="./img/headset1.webp" alt=""/>Headset</li>
+                                <li className="li" ><img src="./img/laptop6.webp" alt="Laptop pic"/>Laptops</li>
+                                <li className="li" ><img src="./img/smartphone6.webp" alt="smartphone pic"/>Smartphones</li>
+                                <li className="li" ><img src="./img/tv8.webp" alt="Tv pic"/>SmartTVs</li>
+                                <li className="li" ><img src="./img/headset1.webp" alt="Headset pic"/>Headset</li>
                             </ul>
                         </nav>
                     </li>
