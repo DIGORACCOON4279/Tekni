27e76fdd2334e24b1cc14374388f41b6460dcb05
DIGORACCOON4279 <dilema4000@gmail.com>
2023-12-23 20:38:36 -0500

    Making Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Storage from "./components/Storage/Storage";
 import Body from "./components/Body/Body";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
@@ -10,6 +10,7 @@ const App = () => {
       <Body/>
       <NavBar/>
       <ItemListContainer greeting=" ⚡ Get 35%  off on all our devices this Black Friday!!! ⚡."/>
+      <Storage/>
     </>
   )
 }
---
+++ src/components/Storage/Storage.jsx
@@ -0,0 +1,327 @@
+import "./Storage.css"
+
+const Storage = () => {
+    return (
+
+        // <main className="container">
+
+            <section className="gallery">
+                <article className="cardProduct">
+                    <img src="img/tv1.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK1-01<span>...$1599</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone1.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone2.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone3.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone4.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone5.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone6.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone7.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone8.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone9.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/smartphone10.webp" alt=""/>
+                    <div className="description">
+                        <h3>Smartphone</h3>
+                        <p>HNK2-02<span>...$799</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop1.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$1099</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv2.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv3.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv4.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv5.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv6.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv7.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv8.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv9.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/tv10.webp" alt=""/>
+                    <div className="description">
+                        <h3>SmartTV</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>dd to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop2.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop3.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop4.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop5.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop6.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop7.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop8.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/laptop9.webp" alt=""/>
+                    <div className="description">
+                        <h3>Laptop</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset3.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset4.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset5.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset6.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset7.webp" alt=""/>
+                    <div className="description">
+                        <h3>NameHeadset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset8.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset9.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset2.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset10.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+                <article className="cardProduct">
+                    <img src="img/headset11.webp" alt=""/>
+                    <div className="description">
+                        <h3>Headset</h3>
+                        <p>HNK3-03<span>...$179</span></p>
+                    </div>
+                    <button>Add to card</button>
+                </article>
+            </section>
+
+        // </main>
+    )
+}
+
+export default Storage
