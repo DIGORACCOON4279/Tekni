fac0b7b005a588ae2b5abce55eb5bf73b6a12720
DIGORACCOON4279 <dilema4000@gmail.com>
2024-01-11 22:36:27 -0500

    Making ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,13 +2,20 @@ import "./ItemDetail.css";
 
 const ItemDetail = ({id, reference, category, price, img, children}) => {
     return (
-        <article>
-            <h3 className="category"> {category} </h3>
-            <p className="id"> ID: {id} </p>
-            <p className="reference"> {reference} <span className="price"> ...${price} </span></p>
-            <img src={img} alt={category} />
-            {children}
-            <p className="description"> Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur earum maxime hic minima sint ab dicta laboriosam, architecto voluptatibus voluptatem quidem deserunt corporis molestiae odit magni perspiciatis, necessitatibus voluptas rerum?</p>
+        <article className="cardItem">
+            <section className="imgCounter">
+                <img className="image" src={img} alt={category} />
+                {children}
+            </section>
+            <section className="categoryInfo">
+                <h3 className="category"> {category} </h3>
+                <div className="info">
+                    <p className="id"> ID: {id} </p>
+                    <p className="reference"> {reference} <span className="price"> ...${price} </span></p>
+                </div>
+                <p className="description"> Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur earum maxime hic minima sint ab dicta laboriosam, architecto voluptatibus voluptatem quidem deserunt corporis molestiae odit magni perspiciatis, necessitatibus voluptas rerum?</p>
+            </section>
+
         </article>
     )
 }
