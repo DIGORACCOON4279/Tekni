a950c1277675223510e1794097949fbfed0513f2
DIGORACCOON4279 <dilema4000@gmail.com>
2024-01-12 00:54:40 -0500

    Updating Itemdetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -3,19 +3,21 @@ import "./ItemDetail.css";
 const ItemDetail = ({id, reference, category, price, img, children}) => {
     return (
         <article className="cardItem">
-            <section className="imgCounter">
+            <figure className="imgCounter">
                 <img className="image" src={img} alt={category} />
-                {children}
-            </section>
+            </figure>
             <section className="categoryInfo">
                 <h3 className="category"> {category} </h3>
                 <div className="info">
                     <p className="id"> ID: {id} </p>
                     <p className="reference"> {reference} <span className="price"> ...${price} </span></p>
+                    <p className="description"> <span className="titleDescription">Description:</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur earum maxime hic minima sint ab dicta laboriosam, architecto voluptatibus voluptatem quidem deserunt corporis molestiae odit magni perspiciatis, necessitatibus voluptas rerum?</p>
                 </div>
-                <p className="description"> <span className="titleDescription">Description:</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur earum maxime hic minima sint ab dicta laboriosam, architecto voluptatibus voluptatem quidem deserunt corporis molestiae odit magni perspiciatis, necessitatibus voluptas rerum?</p>
+                <div className="add">
+                    {children}
+                    <button className="addToCart">Add to cart</button>
+                </div>
             </section>
-
         </article>
     )
 }
