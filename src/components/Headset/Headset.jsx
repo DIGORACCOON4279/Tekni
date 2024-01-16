e80ad889125a39a4a72632db5cea63cd4a4a2c6b
DIGORACCOON4279 <dilema4000@gmail.com>
2024-01-16 15:13:04 -0500

    Updating sliderHeadset

--- src/components/Headset/Headset.jsx
+++ src/components/Headset/Headset.jsx
@@ -3,68 +3,68 @@ import "./Headset.css"
 const Headset = () => {
     return (
         <section className="headsetSection">
-            <article className="info">
+            <section className="info">
                 <h3 className="titleSection">Headset</h3>
                 <p className="description">Dive into Superior Sound Quality and Comfort. Elevate Your Audio Experience with our Precision-Engineered Headsets, Where Every Note and Beat Resonate for a Truly Immersive Journey.</p>
-            </article>
-            <article className="imgHeadsetSection">
-                <div className="headsetSectionUno">
+            </section>
+            <section className="imgHeadsetSection">
+                <div className="headsetSectionOne">
                     <img className="item" src="./img/headsetUno.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Immerse yourself in sound with our headphones.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-                <div className="headsetSectionDos">
+                <div className="headsetSectionTwo">
                     <img className="item" src="./img/headsetDos.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Your music, your world – experience it with our headphones.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-                <div className="headsetSectionTres">
+                <div className="headsetSectionThree">
                     <img className="item" src="./img/headsetTres.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Unleash the power of crystal-clear audio with our headphones.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-                <div className="headsetSectionCuatro">
+                <div className="headsetSectionFour">
                     <img className="item" src="./img/headsetCuatro.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Elevate your listening experience with premium headphones.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-                <div className="headsetSectionCinco">
+                <div className="headsetSectionFive">
                     <img className="item" src="./img/headsetCinco.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Your sound, your style – discover it with our headphones.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-                <div className="headsetSectionSeis">
+                <div className="headsetSectionSix">
                     <img className="item" src="./img/headsetSeis.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Escape into a world of sound with our high-quality headphones.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-                <div className="headsetSectionSiete">
+                <div className="headsetSectionSeven">
                     <img className="item" src="./img/headsetSiete.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Headphones that redefine comfort and audio excellence.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-                <div className="headsetSectionOcho">
+                <div className="headsetSectionEight">
                     <img className="item" src="./img/headsetOcho.webp" alt="" />
                     <section className="btnInfo">
                         <p className="paragraph">Feel the beat, hear the details – choose our headphones.</p>
                         <button className="cta" type="button">See more...</button>
                     </section>
                 </div>
-            </article>
+            </section>
         </section>
     )
 }
