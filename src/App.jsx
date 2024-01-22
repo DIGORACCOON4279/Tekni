b481ed2b2c1be1393a22c0e948ed0799fe691940
DIGORACCOON4279 <dilema4000@gmail.com>
2024-01-22 12:04:30 -0500

    Updating code

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const App = () => {
             <Route path="/category/:idCategory" element={<ItemListContainer/>}/>
             <Route path="/item/:idItem" element={<ItemDetailContainer/>}/>
             <Route path="/" element={<Home/>}/>
-            <Route path="*" element={<Error404/>}/>
+            <Route path="/*" element={<Error404/>}/>
 
             {/* Error404 se le da un enlace para retomar el home */}
           </Routes>
