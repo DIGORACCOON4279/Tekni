e18efcedb0a72ca7a0ff95374cf0cc972db43a0e
DIGORACCOON4279 <dilema4000@gmail.com>
2024-01-21 19:44:58 -0500

    Making Error404Page

--- src/components/Error404/Error404.jsx
+++ src/components/Error404/Error404.jsx
@@ -4,15 +4,16 @@ import { Link } from "react-router-dom";
 const Error404 = () => {
     return (
         <section className="errorSection">
+            <img  className="earth" src="../img/earth.webp" alt="Eath" />
             <div className="image">
                 <img className="astronaut" src="../img/astronaut.webp" alt="Astronaut" />
             </div>
             <div className="info">
-                <h1>
+                <div className="infoSection">
                     <span className="fourZeroFour">4</span>
                     <span className="fourZeroFour">0</span>
                     <span className="fourZeroFour">4</span>
-                </h1>
+                </div>
                 <h4 className="construction">Page not found</h4>
                 <Link className="backToHome" to="/">
                     Back to home
