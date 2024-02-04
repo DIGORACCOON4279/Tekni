724ab742f8515b2f028e588a8a677f3525da6598
DIGORACCOON4279 <dilema4000@gmail.com>
2024-02-03 19:45:10 -0500

    Updating & making shoppingCart when no products & products included

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -384,6 +384,125 @@
 // Con este se va directo a la seccion pero tiene que darle cerrar al shoppingCart
 
 
+// import CartItem from "../CartItem/CartItem";
+// import { Link } from "react-router-dom";
+// import { CartContext } from "../../context/CartContext";
+// import { useContext } from "react";
+// import "./ShoppingCart.css";
+
+// const ShoppingCart = () => {
+//     const { cart, emptyCart, total, quantityTotal, deleteProduct } = useContext(CartContext);
+
+//     return (
+//         <section className="shoppingCartSection">
+//             <label htmlFor="cartWidget">
+//                 <img className="close" src="/img/close.svg" alt="" />
+//             </label>
+//             <h3 className="title">ShoppingCart</h3>
+//             <section className="shoppingItem">
+//                 <h3 className="categoryTitle">Item</h3>
+//                 <h3 className="categoryTitle">Pricing</h3>
+//                 <h3 className="categoryTitle">Quantity</h3>
+//                 <h3 className="categoryTitle">Total</h3>
+//             </section>
+
+//             {quantityTotal === 0 ? (
+//                 <section className="emptyCartSection">
+//                     <img src="../img/emptyCart.gif" alt="" className="emptyCartIcon" />
+//                     <p className="emptyCartNoProducts">No products</p>
+//                     <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products. Enjoy it!!! 🚀</Link>
+//                 </section>
+//             ) : (
+//                 <>
+//                     <section className="shoppingProducts">
+//                         {cart.map(({ item, quantity }) => (
+//                             <CartItem key={item.id} item={item} quantity={quantity} onDelete={() => deleteProduct(item.id)} />
+//                         ))}
+//                     </section>
+//                 </>
+//             )}
+
+//             <section className="emptyCart">
+//                 <div className="emptySection">
+//                     <p className="emptyCartWord">Empty shopping cart</p>
+//                     <img className="emptyCartIcon" onClick={() => emptyCart()} src="/img/trashEmpty.svg" alt="trash icon" />
+//                 </div>
+//                 <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products 🚀</Link>
+//             </section>
+
+//             <section className="totalProduct">
+//                 <p className="sumTotal">Total <span className="numberTotal">...${total}</span></p>
+//                 <Link to="/checkout" className="btnPaymentTotal">Ending shopping</Link>
+//             </section>
+//         </section>
+//     );
+// }
+
+// export default ShoppingCart
+
+
+// import CartItem from "../CartItem/CartItem";
+// import { Link } from "react-router-dom";
+// import { CartContext } from "../../context/CartContext";
+// import { useContext } from "react";
+// import "./ShoppingCart.css";
+
+// const ShoppingCart = () => {
+//     const { cart, emptyCart, total, quantityTotal, deleteProduct } = useContext(CartContext);
+
+//     return (
+//         <section className="shoppingCartSection">
+//             <label htmlFor="cartWidget">
+//                 <img className="close" src="/img/close.svg" alt="" />
+//             </label>
+//             <h3 className="title">ShoppingCart</h3>
+//             <section className="shoppingItem">
+//                 <h3 className="categoryTitle">Item</h3>
+//                 <h3 className="categoryTitle">Pricing</h3>
+//                 <h3 className="categoryTitle">Quantity</h3>
+//                 <h3 className="categoryTitle">Total</h3>
+//             </section>
+
+//             {quantityTotal === 0 ? (
+//                 <section className="emptyCartSection">
+//                     <img src="../img/emptyCart.gif" alt="" className="emptyCartIcon" />
+//                     <p className="emptyCartNoProducts">No products</p>
+//                     <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products. Enjoy it!!! 🚀</Link>
+//                 </section>
+//             ) : (
+//                 <>
+//                     <section className="shoppingProducts">
+//                         {cart.map(({ item, quantity }) => (
+//                             <CartItem key={item.id} item={item} quantity={quantity} onDelete={() => deleteProduct(item.id)} />
+//                         ))}
+//                     </section>
+//                     <section className="totalProduct">
+//                         <p className="sumTotal">Total <span className="numberTotal">...${total}</span></p>
+//                         <Link to="/checkout" className="btnPaymentTotal">Ending shopping</Link>
+//                     </section>
+//                 </>
+//             )}
+
+//             <section className="emptyCart">
+//                 <div className="emptySection">
+//                     <p className="emptyCartWord">Empty shopping cart</p>
+//                     <img className="emptyCartIcon" onClick={() => emptyCart()} src="/img/trashEmpty.svg" alt="trash icon" />
+//                 </div>
+//                 <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products 🚀</Link>
+//             </section>
+//         </section>
+//     );
+// }
+
+// export default ShoppingCart;
+
+
+
+
+
+
+// Este ha sido el mejor codigo adaptado  a lo que necesito
+//////////////////////////////////////////////////////////
 import CartItem from "../CartItem/CartItem";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
@@ -398,43 +517,51 @@ const ShoppingCart = () => {
             <label htmlFor="cartWidget">
                 <img className="close" src="/img/close.svg" alt="" />
             </label>
-            <h3 className="title">ShoppingCart</h3>
-            <section className="shoppingItem">
-                <h3 className="categoryTitle">Item</h3>
-                <h3 className="categoryTitle">Pricing</h3>
-                <h3 className="categoryTitle">Quantity</h3>
-                <h3 className="categoryTitle">Total</h3>
-            </section>
+
 
             {quantityTotal === 0 ? (
-                <section className="emptyCartSection">
-                    <img src="../img/emptyCart.gif" alt="" className="emptyCartIcon" />
-                    <p className="emptyCartNoProducts">No products</p>
-                    <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products</Link>
-                </section>
+                <>
+                    <section className="emptyCartSection">
+                        <img src="../img/emptyCart.gif" alt="" className="emptyCartIcon" />
+                        <p className="emptyCartNoProducts">No products</p>
+                        <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products. Enjoy it!!! 🚀</Link>
+                    </section>
+                </>
             ) : (
                 <>
+                     <h3 className="title">ShoppingCart</h3>
+                    <section className="shoppingItem">
+                        <h3 className="categoryTitle">Item</h3>
+                        <h3 className="categoryTitle">Pricing</h3>
+                        <h3 className="categoryTitle">Quantity</h3>
+                        <h3 className="categoryTitle">Total</h3>
+                    </section>
+
+
                     <section className="shoppingProducts">
                         {cart.map(({ item, quantity }) => (
                             <CartItem key={item.id} item={item} quantity={quantity} onDelete={() => deleteProduct(item.id)} />
                         ))}
                     </section>
+                    {quantityTotal > 0 && (
+                        <section className="emptyCart">
+                            <div className="emptySection">
+                                <p className="emptyCartWord">Empty shopping cart</p>
+                                <img className="emptyCartIcon" onClick={() => emptyCart()} src="/img/trashEmpty.svg" alt="trash icon" />
+                            </div>
+                            <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products 🚀</Link>
+                        </section>
+                    )}
+                    <section className="totalProduct">
+                        <p className="sumTotal">Total <span className="numberTotal">...${total}</span></p>
+                        <Link to="/checkout" className="btnPaymentTotal">Ending shopping</Link>
+                    </section>
                 </>
             )}
 
-            <section className="emptyCart">
-                    <p className="emptyCartWord">Empty shopping cart</p>
-                    <img className="emptyCartIcon" onClick={() => emptyCart()} src="/img/trashEmpty.svg" alt="trash icon" />
-                    <Link to="./ItemListContainer" className="emptyCartLink" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>View products</Link>
 
-            </section>
-
-            <section className="totalProduct">
-                <p className="sumTotal">Total <span className="numberTotal">...${total}</span></p>
-                <Link to="/checkout" className="btnPaymentTotal">Ending shopping</Link>
-            </section>
         </section>
     );
 }
 
-export default ShoppingCart
+export default ShoppingCart;
