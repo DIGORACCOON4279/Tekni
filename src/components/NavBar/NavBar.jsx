e95e88420594dc494dd6a97c1fe21058b6499016
DIGORACCOON4279 <dilema4000@gmail.com>
2024-02-12 20:38:39 -0500

    Updating & fixing navbar margin

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,32 +9,34 @@ import "./NavBar.css"
 const NavBar = () => {
     return (
         <header className="header">
-            <Link to="/" >
-                <img src="/img/logo.webp" alt="" className="logoTekni"/>
-            </Link>
-            <input id="menu" type="checkbox"/>
-            <nav className="navBar">
-                <label htmlFor="menu">
-                </label>
-                <ul>
-                    <NavLink className="navLink" to="/" >
-                        <li className="link">
-                            <img src="/img/home.svg" alt=""/>Home
-                        </li>
-                    </NavLink>
-                    <NavLink className="navLink" to="../itemListContainer" >
-                        <li className="link">
-                            <img src="/img/products.svg" alt=""/>Products
-                        </li>
-                    </NavLink>
-                    <CategorySections/>
-                    <Services/>
-                    <ContactUs />
-                </ul>
-            </nav>
-            <nav className="iconsMenu">
-                <IconsMenu/>
-            </nav>
+            <div className="headerSection">
+                <Link to="/" >
+                    <img src="/img/logo.webp" alt="" className="logoTekni"/>
+                </Link>
+                <input id="menu" type="checkbox"/>
+                <nav className="navBar">
+                    <label htmlFor="menu">
+                    </label>
+                    <ul>
+                        <NavLink className="navLink" to="/" >
+                            <li className="link">
+                                <img src="/img/home.svg" alt=""/>Home
+                            </li>
+                        </NavLink>
+                        <NavLink className="navLink" to="../itemListContainer" >
+                            <li className="link">
+                                <img src="/img/products.svg" alt=""/>Products
+                            </li>
+                        </NavLink>
+                        <CategorySections/>
+                        <Services/>
+                        <ContactUs />
+                    </ul>
+                </nav>
+                <nav className="iconsMenu">
+                    <IconsMenu/>
+                </nav>
+            </div>
         </header>
     )
 }
